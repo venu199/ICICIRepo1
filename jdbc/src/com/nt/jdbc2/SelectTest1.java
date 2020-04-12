5162f67c63e2e702929ed6a329355c1e74c7d03f
gayam <gayam@LAPTOP-CKDR24NQ>
2020-04-12 10:37:16 +0530

    Modify Test1.java

--- jdbc/src/com/nt/jdbc2/SelectTest1.java
+++ jdbc/src/com/nt/jdbc2/SelectTest1.java
@@ -1,9 +1,15 @@
 package com.nt.jdbc2;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class SelectTest1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception{
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		//Establish the Connection 
+		 Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","system","manager");
+		
 
 	}
 
